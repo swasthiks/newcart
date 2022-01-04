2c5fa928b0bc63484ae25ef1504110e31224fedd
swasthik Somayaji <55566745+swasthiks@users.noreply.github.com>
2022-01-04 20:34:30 +0530

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Beligium coffee',
+    price: 50,
+    currency: 'Rs',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Children rainbow',
+    price: 60,
+    currency: 'Rs',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Dilkush',
+    price: 70,
+    currency: 'Rs',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Fruit cocktail',
+    price: 80,
+    currency: 'Rs',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Aloo tikki',
+    price: 40,
+    currency: 'Rs',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'American choconut',
+    price: 40,
+    currency: 'Rs',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
